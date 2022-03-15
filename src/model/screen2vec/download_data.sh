0c54838ad29a376344c74191f81763fff53678bd
pkun <181250068@smail.nju.edu.cn>
2022-03-15 09:21:41 +0800

    delete s2v

--- src/model/screen2vec/download_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-wget https://storage.googleapis.com/crowdstf-rico-uiuc-4540/rico_dataset_v0.1/traces.tar.gz
-tar -xf traces.tar.gz
-
-wget https://storage.googleapis.com/crowdstf-rico-uiuc-4540/rico_dataset_v0.1/unique_uis.tar.gz
-tar -xf unique_uis.tar.gz
