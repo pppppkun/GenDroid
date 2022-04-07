ecbff2431d86f14f5932a74fd17afca37ed363f8
pkun <181250068@smail.nju.edu.cn>
2022-04-07 10:49:10 +0800

    add fine tune .py and successfully fine_tune

--- src/model/bert/fine-tune_sbert.py
+++ src/model/bert/fine-tune_sbert.py
@@ -1,85 +1,42 @@
-"""
-This example loads the pre-trained SentenceTransformer model 'nli-distilroberta-base-v2' from the server.
-It then fine-tunes this model for some epochs on the STS benchmark dataset.
-Note: In this example, you must specify a SentenceTransformer model.
-If you want to fine-tune a huggingface/transformers model like bert-base-uncased, see training_nli.py and training_stsbenchmark.py
-"""
+from sentence_transformers import SentenceTransformer, InputExample, losses, util
 from torch.utils.data import DataLoader
-import math
-from sentence_transformers import SentenceTransformer, LoggingHandler, losses, util, InputExample
-from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
-import logging
-from datetime import datetime
-import csv
-from utils.constant import FINE_TUNE_CALENDAR
 
-logging.basicConfig(format='%(asctime)s - %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    level=logging.INFO,
-                    handlers=[LoggingHandler()])
 
-# Check if dataset exsist. If not, download and extract  it
-# sts_dataset_path = 'datasets/stsbenchmark.tsv.gz'
-#
-# if not os.path.exists(sts_dataset_path):
-#     util.http_get('https://sbert.net/datasets/stsbenchmark.tsv.gz', sts_dataset_path)
+def fine_tune():
+    # Define the model. Either from scratch of by loading a pre-trained model
+    model = SentenceTransformer('all-MiniLM-L6-v2')
 
-# Read the dataset
-model_name = 'all-MiniLM-L6-v2'
-train_batch_size = 16
-num_epochs = 4
-model_save_path = 'output/fine_tune-' + model_name + '-' + datetime.now().strftime(
-    "%Y-%m-%d_%H-%M-%S")
+    # Define your train examples. You need more than just two examples...
+    train_examples = [InputExample(texts=['fill task name', 'resource id is new task name'], label=0.8),
+                      InputExample(texts=['fill task name', 'text is name'], label=0.8),
+                      InputExample(
+                          texts=['fill task description', 'resource id is new task description'],
+                          label=0.8),
+                      InputExample(
+                          texts=['fill task description', 'text is description'],
+                          label=0.8),
+                      InputExample(texts=['fill task name', 'text is description'], label=0.2),
+                      InputExample(texts=['fill task name', 'resource id is new task description'], label=0.2),
+                      InputExample(texts=['fill task description', 'text is name'], label=0.2),
+                      InputExample(texts=['fill task description', 'resource id is new task name'], label=0.2),
+                      InputExample(texts=['fill task information', 'name and description'], label=0.9)]
 
-# Load a pre-trained sentence transformer model
-model = SentenceTransformer(model_name)
+    # Define your train dataset, the dataloader and the train loss
+    train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=16)
+    train_loss = losses.CosineSimilarityLoss(model)
 
-# Convert the dataset to a DataLoader ready for training
-logging.info("Read train dataset")
+    # Tune the model
+    model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=20, warmup_steps=100,
+              output_path='./output_model')
 
-train_samples = []
-dev_samples = []
-test_samples = []
-# with gzip.open(sts_dataset_path, 'rt', encoding='utf8') as fIn:
 
-f = open(FINE_TUNE_CALENDAR, 'r')
-reader = csv.DictReader(f)
-for row in reader:
-    score = float(row['score']) / 5.0  # Normalize score to range 0 ... 1
-    inp_example = InputExample(texts=[row['sentence1'], row['sentence2']], label=score)
+def predict():
+    model = SentenceTransformer('./output_model/')
+    emd1 = model.encode('fill task information')
+    emd2 = model.encode(['name and description', 'name', 'description'])
+    print(util.cos_sim(emd1, emd2))
 
-    if row['split'] == 'dev':
-        dev_samples.append(inp_example)
-    elif row['split'] == 'test':
-        test_samples.append(inp_example)
-    else:
-        train_samples.append(inp_example)
 
-train_dataloader = DataLoader(train_samples, shuffle=True, batch_size=train_batch_size)
-train_loss = losses.CosineSimilarityLoss(model=model)
-
-# Development set: Measure correlation between cosine score and gold labels
-logging.info("Read dev dataset")
-evaluator = EmbeddingSimilarityEvaluator.from_input_examples(dev_samples, name='sts-dev')
-
-# Configure the training. We skip evaluation in this example
-warmup_steps = math.ceil(len(train_dataloader) * num_epochs * 0.1)  # 10% of train data for warm-up
-logging.info("Warmup-steps: {}".format(warmup_steps))
-
-# Train the model
-model.fit(train_objectives=[(train_dataloader, train_loss)],
-          evaluator=evaluator,
-          epochs=num_epochs,
-          evaluation_steps=1000,
-          warmup_steps=warmup_steps,
-          output_path=model_save_path)
-
-##############################################################################
-#
-# Load the stored model and evaluate its performance on STS benchmark dataset
-#
-##############################################################################
-
-model = SentenceTransformer(model_save_path)
-test_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(test_samples, name='sts-test')
-test_evaluator(model, output_path=model_save_path)
+if __name__ == '__main__':
+    fine_tune()
+    predict()
