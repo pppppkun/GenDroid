23650d832744c06d0668a197643d32b3bbb3472d
pkun <181250068@smail.nju.edu.cn>
2022-10-12 14:33:13 +0800

    add process script

--- src/scripts/analyse_log.py
+++ src/scripts/analyse_log.py
@@ -1,18 +1,284 @@
 import os
+import re
+import csv
+import json
+
+import PIL
+from tqdm import tqdm
+from PIL import Image
+from collections import namedtuple
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+    def disable(self):
+        self.HEADER = ''
+        self.OKBLUE = ''
+        self.OKGREEN = ''
+        self.WARNING = ''
+        self.FAIL = ''
+        self.ENDC = ''
+
 
 benchmark = '/Users/pkun/PycharmProjects/ui_api_automated_test/benchmark'
-num = 0
-lines = []
-for f in os.listdir(benchmark):
-    project = os.path.join(benchmark, f)
-    if os.path.isdir(project):
-        for file in os.listdir(project):
-            if file.endswith('.log'):
-                f = open(os.path.join(project, file), 'r').read()
-                f = f.split('\n')
-                for line in f:
-                    if line.startswith('2022'):
-                        if 'valid' in line and 'path' in line and 'successfully' not in line:
-                            lines.append(line)
-                            num += 1
-print(num)
+ablation = '/Users/pkun/PycharmProjects/ui_api_automated_test/ablation'
+# apps = ['chrome', 'contact', 'gmail', 'photo', 'setting', 'clock']
+apps = list(filter(lambda x: os.path.isdir(os.path.join(benchmark, x)), os.listdir(benchmark)))
+TIME = re.compile(r'\d+-\d+-(\d+) (\d+):(\d+):\d+')
+STATEMENT = re.compile(r'd\(.*\).*')
+SCREENSHOT = re.compile(r'screenshot is (\d+.png)')
+MATCH_WIDGET = re.compile(r'executor - INFO - match widget ')
+DESCRIPTION = re.compile('generating event for "(.*)"')
+format_time = namedtuple('format_time', ['day', 'hour', 'minute'])
+data_header = ['app', 'filename', 'time', 'events']
+time_dst_file = 'data_with_time.csv'
+event_dst_file = 'data_with_events.csv'
+dynamic_match_file = 'static_dm.json'
+exceptions_log = []
+
+
+def write_csv(rows, dst_file):
+    with open(dst_file, 'w') as f:
+        f_csv = csv.DictWriter(f, data_header)
+        f_csv.writeheader()
+        f_csv.writerows(rows)
+
+
+def get_app_and_filename(filepath: str):
+    app = None
+    for app in apps:
+        if app in filepath:
+            break
+    rmost = filepath.rfind('/')
+    filename = filepath[rmost:]
+    return app, filename
+
+
+def to_format_row(data: dict):
+    rows = []
+    for filepath in data:
+        d = dict()
+        app, filename = get_app_and_filename(filepath)
+        d['app'] = app
+        d['filename'] = filename
+        d.update(data[filepath])
+        rows.append(d)
+    return rows
+
+
+def iterator_log(dataset):
+    for app in apps:
+        d = os.path.join(dataset, app)
+        for filename in os.listdir(d):
+            if filename.endswith('.log'):
+                filepath = os.path.join(d, filename)
+                test_item = filepath[:-7]
+                log = open(filepath).read().strip().split('\n')
+                yield test_item, log
+
+
+def iterator_test(dataset):
+    for app in apps:
+        d = os.path.join(dataset, app)
+        for filename in os.listdir(d):
+            if filename.endswith('.py') and (filename.startswith('test_') or filename.startswith('Itest')):
+                filepath = os.path.join(d, filename)
+                test_item = filepath[:-3]
+                test = open(filepath).read().strip().split('\n')
+                yield test_item, test
+
+
+def get_time(line):
+    m = TIME.match(line)
+    time = None
+    if m:
+        time = format_time(int(m.group(1)), int(m.group(2)), int(m.group(3)))
+    return time
+
+
+def calculate_time(log):
+    start_time = get_time(log[0])
+    end_time = get_time(log[-1])
+    time = -1
+    if start_time and end_time:
+        if start_time.day == end_time.day:
+            if end_time.hour == start_time.hour:
+                time = end_time.minute - start_time.minute
+            else:
+                if end_time.hour - start_time.hour < 2:
+                    time = (end_time.hour - start_time.hour) * 60 + end_time.minute - start_time.minute
+
+    return time
+
+
+def analysis_time(dataset):
+    result = dict()
+    for item, log in iterator_log(dataset):
+        time = calculate_time(log)
+        if time == -1:
+            exceptions_log.append(item)
+        result[item] = {'time': time}
+    print('\n'.join(exceptions_log))
+    return result
+
+
+def get_events(test: list):
+    ntest = []
+    i = 4
+    while i != len(test):
+        if 'com.google.android.inputmethod.latin' in test[i]:
+            i += 2
+            continue
+        if 'd.sleep(3)' in test[i]:
+            i += 1
+            continue
+        ntest.append(test[i])
+        i += 1
+    return ntest
+
+
+def analysis_events(dataset):
+    result = dict()
+    for item, test in iterator_test(dataset):
+        events = get_events(test)
+        if len(events) == 0:
+            exceptions_log.append(item)
+        result[item] = {'events': len(events)}
+    print('\n'.join(exceptions_log))
+    return result
+
+
+def iterator_dynamic_match(log):
+    screenshots = []
+    match_widgets = []
+    descriptions = []
+    for line in log:
+        screenshot = SCREENSHOT.findall(line)
+        description = DESCRIPTION.findall(line)
+        if len(screenshot) != 0:
+            screenshots += screenshot
+        if len(description) != 0:
+            descriptions += description
+        if 'executor - INFO - match widget' in line:
+            offset = len('match widget')
+            index = line.find('match widget')
+            match_widgets.append(line[index + offset + 1:])
+    return screenshots, match_widgets, descriptions
+
+
+def analysis_screenshot(dataset):
+    total = dict()
+    for filepath, log in iterator_log(dataset):
+        screenshots, match_widgets, description = iterator_dynamic_match(log)
+        if len(screenshots) != len(match_widgets):
+            exceptions_log.append(filepath)
+        else:
+            s_ms = []
+            for s, m, d in zip(screenshots, match_widgets, description):
+                s_m = dict()
+                s_m['screenshot'] = s
+                s_m['match_widget'] = m
+                s_m['description'] = d
+                s_ms.append(s_m)
+            total[filepath] = s_ms
+
+    json.dump(total, open(dynamic_match_file, 'w'), indent=4)
+
+
+def get_screen_folder(log_filepath):
+    rmost = log_filepath.rfind('/')
+    parent = log_filepath[:rmost]
+    screen_folder = os.path.join(parent, 'screenshots')
+    return screen_folder
+
+
+def show_image(filename):
+    img = Image.open(filename)
+    img.show()
+    img.close()
+
+
+def parser_input(clazz):
+    d = {'y': True, 'n': False, 'e': 'Error Screen'}
+    return d[clazz]
+
+
+def manual_mark_dm():
+    dm = json.load(open(dynamic_match_file, 'r'))
+    count = 0
+    try:
+        for filepath in dm:
+            screen_and_widget = dm[filepath]
+            screen_folder = get_screen_folder(filepath)
+            for item in screen_and_widget:
+                if 'class' not in item:
+                    screen_filepath = os.path.join(screen_folder, item['screenshot'])
+                    try:
+                        show_image(screen_filepath)
+                    except PIL.UnidentifiedImageError:
+                        continue
+                    print(
+                        f'\n{filepath}\n{bcolors.OKBLUE}widget: {item["match_widget"]}{bcolors.ENDC} {bcolors.OKGREEN}description: {item["description"]}{bcolors.ENDC}')
+                    clazz = input('input class[y/n/e/q]  ')
+                    if clazz == 'q':
+                        json.dump(dm, open(dynamic_match_file, 'w'), indent=4)
+                        print(f'{count} / {len(dm)}')
+                        exit(1)
+                    clazz = parser_input(clazz)
+                    item['class'] = clazz
+            count += 1
+    except Exception:
+        json.dump(dm, open(dynamic_match_file, 'w'), indent=4)
+        print(f'{count} / {len(dm)}')
+        exit(1)
+
+
+def read_csv(filename):
+    from collections import namedtuple
+    with open(filename) as f:
+        f_csv = csv.reader(f)
+        headings = next(f_csv)
+        Row = namedtuple('Row', headings)
+        for r in f_csv:
+            yield Row(*r)
+
+
+if __name__ == '__main__':
+    time_result = analysis_time(ablation)
+    # rows = to_format_row(result)
+    # write_csv(rows, time_dst_file)
+    event_result = analysis_events(ablation)
+    result = {}
+    for file in time_result.keys():
+        time = time_result[file]
+        event = event_result[file]
+        r = {}
+        r.update(time)
+        r.update(event)
+        result[file] = r
+    rows = to_format_row(result)
+    write_csv(rows, 'ablation_record.csv')
+    # manual_mark_dm()
+    # dm = json.load(open(dynamic_match_file))
+    # T = 0
+    # F = 0
+    # E = 0
+    # for file in dm:
+    #     for swd in dm[file]:
+    #         if type(swd['class']) == bool:
+    #             if swd['class']:
+    #                 T += 1
+    #             else:
+    #                 F += 1
+    #         else:
+    #             E += 1
+    # total = T + F + E
+    # print(total)
+    # print(T/total, F/total, E/total)
+    #
--- src/scripts/convert.py
+++ src/scripts/convert.py
@@ -1,6 +1,12 @@
+import json
 import os
+from functools import reduce
+import csv
+from collections import namedtuple
 from xml.etree import ElementTree as et
-from demo.event import Event
+
+
+# from gendroid.confidence import postprocess_keys, get_attribute_base_on_class
 
 
 def bounds2list(bounds):
@@ -40,18 +46,112 @@ def gui_xml2json(xml: et, activity_name):
     return result
 
 
-def gui_similarity_matrix(record):
-    pass
+# def trace2dataset(trace_file):
+#     traces = json.load(trace_file)
+#     total_description = traces['description']
+#     atomic_description_and_widget = []
+#     for trace in traces['trace']:
+#         atomic_description = trace['description']
+#         widget = trace['widget']
+#         keys = get_attribute_base_on_class(widget)
+#         keys = postprocess_keys(keys)
+#         widget_semantic = keys
+#         atomic_description_and_widget.append([atomic_description, widget])
 
 
-def to_scripts():
-    scripts = """
-import uiautomator2 as u2
-d = u2.connect()
-"""
-    event = Event(action='click', selector={'text':'123', 'content-desc':'licas', 'resource-id':'aljchas'})
-    print(event.to_uiautomator2_format())
+def helper(file_path):
+    import csv
+    from collections import namedtuple
+    rows = []
+    with open(file_path) as f:
+        f_csv = csv.reader(f)
+        headings = next(f_csv)
+        Row = namedtuple('Row', headings)
+        for r in f_csv:
+            row = Row(*r)
+            rows.append(row)
+            # Process row
+    return rows
+
+
+def fetch_origin_script(direction):
+    scripts = filter(lambda x: x.endswith('.py'), os.listdir(direction))
+    scripts = filter(lambda x: x != 'runner.py', scripts)
+    scripts = filter(lambda x: not x.startswith('test_') and not x.startswith('_test'), scripts)
+    scripts = list(scripts)
+    return scripts
+
+
+def delete_evaluate_data():
+    direction = '/Users/pkun/PycharmProjects/ui_api_automated_test/ablation'
+    apps = os.listdir(direction)
+    apps = filter(lambda x: os.path.isdir(os.path.join(direction, x)), apps)
+    files = map(lambda x: (x, os.listdir(os.path.join(direction, x))), apps)
+    for app, need_to_remove in files:
+        need_to_remove = filter(
+            lambda f: f.startswith('test_') or f.startswith('Itest_') or f == 'screenshots' or f.startswith('_test'),
+            need_to_remove)
+        for file in need_to_remove:
+            os.system(f'rm -r {os.path.join(direction, app, file)}')
+
+
+def make_runner():
+    direction = '/Users/pkun/PycharmProjects/ui_api_automated_test/ablation'
+    runner_format = 'import subprocess\nimport os\nfilepaths= [\n    {filepaths}\n]\nfor filepath in filepaths:\n    subprocess.call(["python3", filepath])'
+    apps = os.listdir(direction)
+    apps = filter(lambda x: os.path.isdir(os.path.join(direction, x)), apps)
+    scripts_tuple = map(lambda x: (x, fetch_origin_script(os.path.join(direction, x))), apps)
+    for app, scripts in scripts_tuple:
+        filepaths = []
+        for script in scripts:
+            filepaths.append(script)
+        filepaths = ',\n    '.join([repr(filepath) for filepath in filepaths])
+        runner = runner_format.format(filepaths=filepaths)
+        target = os.path.join(direction, app, 'runner.py')
+        f = open(target, 'w+')
+        f.write(runner)
+
+
+def write_csv(data_header, rows, dst_file):
+    with open(dst_file, 'w') as f:
+        f_csv = csv.DictWriter(f, data_header)
+        f_csv.writeheader()
+        f_csv.writerows(rows)
+
+
+def recovery_pixel_help():
+    processed = '/Users/pkun/PycharmProjects/ui_api_automated_test/src/model/bert/processed.csv'
+    origin = '/Users/pkun/PycharmProjects/ui_api_automated_test/src/model/bert/origin_step.txt'
+    count = 0
+    checkpoint = 0
+    origin = open(origin).read().split('\n')
+    new_rows = []
+    Row = namedtuple('Row', ['id', 'description', 'steps', 'category', 'script', 'reason'])
+    f = open(processed)
+    f_csv = csv.reader(f)
+    headings = next(f_csv)
+    for r in f_csv:
+        if count < checkpoint:
+            count += 1
+            continue
+        row = Row(*r)
+        if row.script == 'yes':
+            print(count)
+            print(origin[count])
+            print(row.steps)
+            before = input()
+            if before == 'exit':
+                break
+            new_steps = before + row.steps
+            new_row = Row(id=row.id, description=row.description, steps=new_steps, script=row.script,
+                          reason=row.reason, category=row.category)
+            new_rows.append(new_row._asdict())
+        else:
+            new_rows.append(row._asdict())
+        count += 1
+    print(checkpoint)
+    write_csv(headings, new_rows, 'recovery.csv')
 
 
 if __name__ == '__main__':
-    to_scripts()
+    pass
